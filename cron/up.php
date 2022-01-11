05b137f19b83e589a2eb99986406df164e12469c
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-11 19:36:23 +0100

    a

--- cron/up.php
+++ cron/up.php
@@ -15,7 +15,7 @@ $result = mysqli_query($conn, $sql);
 
 var_dump($result);
 
-while ($row = mysql_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) {
   echo "<pre>";
   echo $row["url_id"];
 }
