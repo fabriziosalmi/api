1d394bcf51fe1361c178c3f1cbbb51f5a68868b2
fab <fabrizio.salmi@gmail.com>
2022-01-11 13:21:59 +0100

    d

--- url.php
+++ url.php
@@ -3,7 +3,10 @@ require_once("conf/database.php");
 
 $url = filter_var($_POST['url'], FILTER_VALIDATE_URL);
 
+var_dump($url);
 $sql = "INSERT INTO urls (url) VALUES (\'$url\');";
+var_dump($sql);
+
 
 $result = mysql_query($sql);
     if($result){
