a5bc203932320a22fe755126b22c838bd1d7aab4
fab <fabrizio.salmi@gmail.com>
2022-01-11 14:12:18 +0100

    better url input filters

--- url.php
+++ url.php
@@ -3,6 +3,30 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+$url = $_POST['url'];
+$url = filter_var($url, FILTER_VALIDATE_URL);
+
+$url_check = parse_url($url);
+
+$scheme = $url_check["scheme"];
+$host = $url_check["host"];
+$path = $url_check["path"];
+$fragment = $url_check["fragment"];
+
+if ( $scheme != "https") {
+    die("https required");
+}
+
+if ( $path != "") {
+    die("base url required (path given)");
+}
+
+if ( $fragment != "") {
+    die("base url required (fragment given)");
+}
+
+$url = $scheme."://".$host;
+
 require_once("conf/database.php");
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,8 +34,6 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$url = $_POST['url'];
-$url = filter_var($url, FILTER_VALIDATE_URL);
 $sql = "INSERT INTO urls (url,status) VALUES ('".$url."', 0);";
 
 if(mysqli_query($conn, $sql)){
