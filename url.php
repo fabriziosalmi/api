da30513973f11774d1760cd2319db3f99c15fa11
fab <fabrizio.salmi@gmail.com>
2022-01-11 13:28:58 +0100

    finally :D

--- url.php
+++ url.php
@@ -8,13 +8,9 @@ require_once("conf/database.php");
 
 $url = filter_var($_POST['url'], FILTER_VALIDATE_URL);
 
-print_r($url);
-
 $sql = "INSERT INTO urls (url) VALUES (\'$url\');";
-var_dump($sql);
 
-
-$result = mysql_query($sql);
+$result = $conn->mysql_query($sql);
 
     if($result){
 	    echo("ok");
