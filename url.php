3a8655bccdb74a6607b90b9f7437fa6315cbfe33
fab <fabrizio.salmi@gmail.com>
2022-01-11 13:26:25 +0100

    a

--- url.php
+++ url.php
@@ -1,9 +1,15 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
 require_once("conf/database.php");
 
 $url = filter_var($_POST['url'], FILTER_VALIDATE_URL);
 
-var_dump($url);
+print_r($url);
+
 $sql = "INSERT INTO urls (url) VALUES (\'$url\');";
 var_dump($sql);
 
