65129443776c81b6d1c48aeac942fe4c58dfd8fd
fabriziosalmi <root@vmi574892.contaboserver.net>
2022-01-11 13:47:28 +0100

    testing

--- url.php
+++ url.php
@@ -22,9 +22,9 @@ var_dump($sql);
 if(mysqli_query($conn, $sql)){
     echo "Records inserted successfully.";
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
  
-mysqli_close($link);
+mysqli_close($conn);
 
 ?>
