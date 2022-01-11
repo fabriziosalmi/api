caf86485793b79042a7e4a228ee4c34bd929a57a
fab <fabrizio.salmi@gmail.com>
2022-01-11 13:31:51 +0100

    g

--- url.php
+++ url.php
@@ -3,14 +3,19 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-
 require_once("conf/database.php");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
 
 $url = filter_var($_POST['url'], FILTER_VALIDATE_URL);
 
 $sql = "INSERT INTO urls (url) VALUES (\'$url\');";
 
-$result = $conn->mysql_query($sql);
+$result = $conn->query($sql);
 
     if($result){
 	    echo("ok");
