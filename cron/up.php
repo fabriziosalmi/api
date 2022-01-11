7b744ec761dfb9af78fcdddfa2e7104d8370d81e
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-11 20:09:09 +0100

    a

--- cron/up.php
+++ cron/up.php
@@ -14,13 +14,15 @@ if ($conn->connect_error) {
 $sql = "SELECT url_id FROM monitor_link WHERE monitor_id = 1 AND status = 1;";
 $result = mysqli_query($conn, $sql);
 
+$id_urls[] = "";
+
 while ($row = mysqli_fetch_assoc($result)) {
   var_dump($row["url_id"]);
-  // $id_urls = $row["url_id"];
+  $id_urls += $row["url_id"];
 }
 
 
-
+var_dump($id_urls);
 
 
 ?>
