dc0208de35a0197e415cf8dadd282acb3c3fc001
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-12 12:32:30 +0100

    a

--- cron/up.php
+++ cron/up.php
@@ -21,5 +21,15 @@ foreach ($stmt->get_result() as $row)
 }
 print_r($array);
 
+foreach ($array as $url_id_to_monitor) {
+
+    $sql1 = "SELECT url FROM urls WHERE url_id = '".$url_id_to_monitor."';";
+    $result1 = $conn->query($sql1);
+    $row1 = $result1->fetch_assoc();
+    $url = $row["url"];
+    $shell_cmd = "/usr/bin/wget -q -O https://charts.rivoluzioneinformatica.org/api/up.php?url=".$url."";
+    shell_exec($shell_cmd);
+
+}
 
 ?>
