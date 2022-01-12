8b5cb6df17c07797e62a45578cb9e370f1728c27
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-12 12:57:27 +0100

    a

--- cron/up.php
+++ cron/up.php
@@ -25,6 +25,7 @@ foreach ($array as $url_id_to_monitor) {
 
     $sql_url = "SELECT url FROM urls WHERE id = ".$url_id_to_monitor.";"; 
     $result1 = $conn->query($sql_url);
+    var_dump($result1);
     $row1 = $result1->fetch_assoc();
     var_dump($row);
 
