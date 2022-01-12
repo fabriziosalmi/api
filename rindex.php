4a5abf33261c05b594bb2a45c0295903be809675
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-12 13:51:36 +0100

    rindex

---
+++ rindex.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require_once("conf/database.php");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql_count_urls = "SELECT COUNT(id) FROM url;";
+$result_0 = $conn->query($sql_count_urls);
+$urls_count = $result_0->fetch_assoc();
+$urls_count = $urls_count["COUNT(id)"];
+
+$sql_param_1 = "SELECT AVG(score) FROM scores WHERE url_id IN(SELECT DISTINCT url_id FROM urls) ORDER BY id DESC LIMIT ".$urls_count.";";
+$result_1 = $conn->query($sql_param_1);
+$result_1 = $result_1->fetch_assoc();
+$RI_score = floatval($result_1["AVG(score)"]);
+
+$sql1 = "INSERT INTO rindex (score) VALUES ('".$RI_score."')";
+
+if ($conn->query($sql1) === TRUE) {
+  echo "New record created successfully - [ ".$sql1." ]";
+} else {
+  echo "Error: " . $sql1 . "<br>" . $conn->error;
+}
