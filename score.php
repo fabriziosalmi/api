6bf60600c52178037d7a12564d924aef414f03b8
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-12 14:37:56 +0100

    score

--- score.php
+++ score.php
@@ -20,7 +20,7 @@ if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
   echo("<br> $url is invalid"); 
 } 
 
-require_once("../conf/database.php");
+require_once("conf/database.php");
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -33,8 +33,8 @@ $url_id = $result->fetch_assoc()["id"];
 
 print_r("<br>url id: ".$url_id."<br>");
 
-$sql_param = "SELECT score FROM checks WHERE url_id = ".$url_id." AND monitor_id = 1 ORDER BY id DESC LIMIT 1;";
-$result1 = $conn->query($sql_param);
+$sql_param1 = "SELECT score FROM checks WHERE url_id = ".$url_id." AND monitor_id = 1 ORDER BY id DESC LIMIT 1;";
+$result1 = $conn->query($sql_param1);
 if ($result1->num_rows > 0) { while($row = $result1->fetch_assoc()) { $score = $row["score"]; } } else { $score = $row["score"]; }
 $url_score = $score;
 
@@ -49,7 +49,7 @@ $dns_score = $score;
 print_r("<br>dns: ".$dns_score."<br>");
 
 // sum scores - to improve with sql select result
-$sum = $url_score  + $dns_score;
+$sum = $url_score + $dns_score;
 $values=2;
 $score = floatval($sum) / floatval($values);
 
