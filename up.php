332893470a26969396dea0bf543d52c890396937
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-12 18:52:32 +0100

    util

--- up.php
+++ up.php
@@ -2,32 +2,6 @@
 require_once("conf/logging.php");
 require_once("util.php");
 
-if (PHP_SAPI === 'cli')
-{
-    parse_str(implode('&', array_slice($argv, 1)), $_GET);
-} 
-
-$url = $_GET['url'];
-// test
-$url = filter_var($url, FILTER_SANITIZE_URL);
-
-if (!filter_var($url, FILTER_VALIDATE_URL) === false) { 
-  echo "<br> ".$url." is valid"; 
-} else { 
-  die("<br> $url is invalid"); 
-} 
-
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HEADER, true); 
-curl_setopt($ch, CURLOPT_NOBODY, true);    
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_TIMEOUT,10);
-curl_setopt($ch, CURLINFO_NAMELOOKUP_TIME, true);
-curl_setopt($ch, CURLINFO_CONNECT_TIME, true);
-
-$output = curl_exec($ch);
-
 $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 curl_close($ch);
