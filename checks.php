7f2577c3bbfb481aa96f340860cb9564677668a6
fabriziosalmi <fabrizio.salmi@gmail.com>
2022-01-14 21:03:27 +0100

    html compliance

--- checks.php
+++ checks.php
@@ -1,5 +1,9 @@
-<html>
-    <head>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>url</title>
         <style>
             #scores {
                 font-family: Arial, Helvetica, sans-serif;
